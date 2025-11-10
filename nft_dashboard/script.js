a127a7b01116984d9b21214d2de2408e869d2d79
badr <badrsama300@gmail.com>
2025-11-10 17:04:44 +0100

    NFT

---
+++ nft_dashboard/script.js
@@ -0,0 +1,12 @@
+function left(){
+    const scroll = document.getElementById("scroll");
+    scroll.scrollBy(-250,0);
+    console.log(1);
+    
+}
+function right(){
+    const scroll = document.getElementById("scroll");
+    scroll.scrollBy(250,0);
+    console.log(2);
+    
+}
